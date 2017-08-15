de46d9afd29fd162e060ae8187e3847d7ed09b19
Gapur Kassym <gapur.kassym@gmail.com>
2017-08-15 11:17:03 +0600

    removed folder controllers

--- baking_api/api/routes/noteRoutes.js
+++ baking_api/api/routes/noteRoutes.js
@@ -1,11 +1,22 @@
-const noteController = require('../controllers/noteController');
+const mongose = require('mongose');
+const Note = require('../models/note');
 
 module.exports = function(app) {
   app.get('/', (req, res) => {
     res.send("<h2>Привет Express!</h2>");
   });
 
-  app.get('/notes', noteController.getNotes);
+  app.get('/notes', (req, res) => {
+    Note.find({}, (err, notes) => {
+      if (err) res.send(err);
+      res.json(notes);
+    });
+  });
 
-  app.get('/notes/:id', noteController.getNoteById);
+  app.get('/notes/:id', (req, res) => {
+    Note.findById(req.params.NoteId, (err, note) => {
+      if (err) res.send(err);
+      res.json(note);
+    });
+  });
 };
