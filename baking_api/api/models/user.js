d6cbb36f6b4c6bb68e07e851f5ab5c1019e0505d
Gapur Kassym <gapur.kassym@gmail.com>
2017-08-20 01:08:33 +0600

    user role

--- baking_api/api/models/user.js
+++ baking_api/api/models/user.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
-const User = new Schema({
+const UserSchema = new Schema({
   email: {
     type: String,
     validate: /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/,
     required: true,
     unique: true,
+    lowercase: true,
   },
   password: {
     type: String,
     required: true
   },
+  role: {
+    type: String,
+    enum: ['Client', 'Admin'],
+    default: 'Client'
+  },
 }, { versionKey: false });
 
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model('User', UserSchema);
