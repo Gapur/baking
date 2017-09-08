1314a0eba7039201719528e83c93c374ea40351e
Gapur Kassym <gapur.kassym@gmail.com>
2017-09-08 14:47:04 +0600

    login screen style

--- baking_web/src/Router.js
+++ baking_web/src/Router.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import NotesScreen from './notes/NotesScreen';
+import LoginScreen from './auth/LoginScreen';
+import SignupScreen from './auth/SignupScreen';
 import NotFoundScreen from './shared/components/NotFoundScreen';
 
 const RouterComponent = () =>
   <Switch>
     <Route exact path="/" component={NotesScreen} />
     <Route exact path="/notes" component={NotesScreen} />
+    <Route exact path='/login' component={LoginScreen} />
     <Route path="/" component={NotFoundScreen} />
   </Switch>;
 
