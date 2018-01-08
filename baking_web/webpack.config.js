78dbc7c86a9d97b4debb9bb8a27b895b8dd7b5ea
Gapur Kassym <gapur.kassym@gmail.com>
2018-01-08 17:31:23 +0600

    style structure

--- baking_web/webpack.config.js
+++ baking_web/webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    path.resolve(__dirname, 'src/app.scss'),
+    path.resolve(__dirname, 'src/index.js'),
+  ],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
@@ -18,7 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules(?!\/webpack-dev-server)/,
         use: {
           loader: 'babel-loader',
           options: {
@@ -28,7 +31,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract('css-loader', 'style-loader')
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        })
+      },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules)/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
       },
       {
         test: /\.(woff2?|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
