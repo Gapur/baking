87008997d2374cf351a8b4e0f9feea439afdb335
Gapur Kassym <gapur.kassym@gmail.com>
2018-01-14 00:05:23 +0600

    changes action and reducer recipes

--- baking_web/src/shared/constants/actionTypes.js
+++ baking_web/src/shared/constants/actionTypes.js
@@ -7,3 +7,8 @@ export const LOGIN = 'LOGIN';
 export const SIGNUP = 'SIGNUP';
 export const UPDATE_ACCOUNT = 'UPDATE_ACCOUNT';
 export const LOGOUT = 'LOGOUT';
+
+export const FETCH_RECIPES = 'FETCH_RECIPES';
+export const CREATE_RECIPE = 'CREATE_RECIPE';
+export const UPDATE_RECIPE = 'UPDATE_RECIPE';
+export const DELETE_RECIPE = 'DELETE_RECIPE';
